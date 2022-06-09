98de7e422a2fffe15645a34382d267fd0d440973
Jinyue Yuan <jinyueyu@usc.edu>
2022-06-09 05:59:26 +0000

    fix spacing in script.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,39 +16,39 @@
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-    const greetings =
-        ['Born and Raised in Shenzhen, China', 'Have a 960+ day Duolingo streak but do not speak a third language', 'I Love Nature', 'Considering moving to NYC because driving is not necessary there', 'My family has a Shiba Inu, he is turning 2 this summer!'];
+  const greetings =
+    ['Born and Raised in Shenzhen, China', 'Have a 960+ day Duolingo streak but do not speak a third language', 'I Love Nature', 'Considering moving to NYC because driving is not necessary there', 'My family has a Shiba Inu, he is turning 2 this summer!'];
   
-    // Pick a random greeting.
-    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick a random greeting.
+  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
   
-    // Add it to the page.
-    const greetingContainer = document.getElementById('greeting-container');
-    greetingContainer.innerText = greeting;
-  }
+  // Add it to the page.
+  const greetingContainer = document.getElementById('greeting-container');
+  greetingContainer.innerText = greeting;
+}
   
-  function changeSlideImg() {
-    const images = 
+function changeSlideImg() {
+  const images = 
     [
       'images/hobby-pic1.JPG',
       'images/hobby-pic2.JPEG',
       'images/hobby-pic3.jpg'
     ]
   
-    // Pick a random image.
-    const getImg = images[Math.floor(Math.random() * images.length)];
+  // Pick a random image.
+  const getImg = images[Math.floor(Math.random() * images.length)];
   
-    // Add it to the page.
-    const setImg = document.getElementById('slideImg');
-    setImg.src = getImg;
-  }
+  // Add it to the page.
+  const setImg = document.getElementById('slideImg');
+  setImg.src = getImg;
+}
   
 
 // Fetches welcome message and add to front page
 async function showWelcomeMsg() {
-    const responseData = await fetch('/hello');
-    const textFromResponse = await responseData.text();
+  const responseData = await fetch('/hello');
+  const textFromResponse = await responseData.text();
 
-    const msgContainer = document.getElementById('welcome-msg-container');
-    msgContainer.innerHTML = textFromResponse;
+  const msgContainer = document.getElementById('welcome-msg-container');
+  msgContainer.innerHTML = textFromResponse;
 }
