0210e276a2d0a36f9ba118097452c76db5f3bd27
Jinyue Yuan <jinyueyu@usc.edu>
2022-06-10 02:41:48 +0000

    display message through fetch messages in servlet

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,18 +1,50 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import com.google.gson.Gson; 
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
+  // Array that stores strings 
+  private ArrayList<String> messages = new ArrayList<String>();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello! Welcome to Emma's Portfolio:) Remember to have a nice day.</h1>");
+
+    messages.add("Just stop your crying, it is a sign of the times");
+    messages.add("Welcome to the final show");
+    messages.add("Hope you are wearing your best clothes");
+    messages.add("You cannot bribe the door on your way to the sky");
+    messages.add("You look pretty good down here");
+    messages.add("We never learn, we have been here before");
+    messages.add("Why are we always stuck and running from the bullets?");
+    messages.add("Why are we always stuck and running from your bullets?");
+    messages.add("We gotta get away from here");
+    messages.add("Just stop your crying it will be alright");
+
+    String json = convertToJsonUsingGson(messages);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+ /**
+   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
+   * the Gson library dependency to pom.xml.
+   */
+  private String convertToJsonUsingGson(ArrayList arr) {
+    Gson gson = new Gson();
+    String json = gson.toJson(arr);
+    return json;
   }
 }
