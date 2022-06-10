7786ccff97734728af03660387fa786181ed4211
Jinyue Yuan <jinyueyu@usc.edu>
2022-06-10 18:09:46 +0000

    change import order

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,10 +1,8 @@
 package com.google.sps.servlets;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.ArrayList;
 import com.google.gson.Gson; 
-
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
